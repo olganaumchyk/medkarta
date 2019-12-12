469ce01c216a1821555a152204bf3503e2f1a152
olganaumchyk <47150888+olganaumchyk@users.noreply.github.com>
2019-12-12 20:43:53 +0300

    ашт2

--- src/Model/PrintModel.php
+++ src/Model/PrintModel.php
@@ -16,13 +16,5 @@ class PrintModel extends DbEntity
        
     }
 
-    public function searchpriv(string $name = ''): array
-    {
-
-        return $this->runSQL("SELECT `privivki`.`name` AS`privivki_id`, `patients`.`fio` AS`patients_id`,birth_date,adres,date,vaccine
-        FROM `karta`,`privivki`,`patients`
-        WHERE `karta`.`patients_id` = `patients`.`id` AND `karta`.`privivki_id` =`privivki`.`id`AND `privivki`.`name` LIKE '%$name%'");
-       
-    }
     
 }
