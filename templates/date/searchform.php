517ce45a959765a4cf2c50253d4a060120d204c5
olganaumchyk <47150888+olganaumchyk@users.noreply.github.com>
2019-12-18 17:05:28 +0300

    f3

--- templates/date/searchform.php
+++ templates/date/searchform.php
@@ -1,6 +1,6 @@
 <div>
     <form action="<?= $URL ?>" method="POST" class="text-center border border-light p-5">
-        <input type="text" name="date" placeholder="введите дату вакцинации"></br></br></br>
+        <input type="date" name="date" placeholder="введите дату вакцинации"></br></br></br>
         <input type="submit" value="поиск">
 
     </form>
